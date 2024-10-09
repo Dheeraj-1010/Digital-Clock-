9eab04cf861fc54789c6e5ecdfaf74eaaa2c975d
Dheeraj yadav <dheerajyadavgunawal@gmail.com>
2024-10-09 12:47:06 +0530

    main.py

--- main.py
+++ main.py
@@ -28,50 +28,50 @@ clock = Tk()
 # configure main panel
 clock.title('    *** Digital Clock ***')
 clock.geometry('1000x500')
-clock.config(bg = 'DCE0D9')
+clock.config(bg = '#DCE0D9')
 
 
 #  Time
-lab_hr = Label(clock, text="00",font=('Time New Roman', 60, "bold"), bg='EAD7C3', fg="white")
+lab_hr = Label(clock, text="00",font=('Time New Roman', 60, "bold"), bg='#EAD7C3', fg="white")
 lab_hr.place(x=120, y=50, height=110, width=100)
-lab_hr_txt = Label(clock, text="HOUR",font=('Time New Roman', 20, "bold"), bg='FBF6EF', fg="white")
+lab_hr_txt = Label(clock, text="HOUR",font=('Time New Roman', 20, "bold"), bg='#FBF6EF', fg=" Black")
 lab_hr_txt.place(x=120, y=190, height=40, width=100)
 
-lab_min = Label(clock, text="00",font=('Time New Roman', 60, "bold"), bg='EAD7C3', fg="white")
+lab_min = Label(clock, text="00",font=('Time New Roman', 60, "bold"), bg='#EAD7C3', fg="white")
 lab_min.place(x=340, y=50, height=110, width=100)
-lab_min_txt = Label(clock, text="MIN.",font=('Time New Roman', 20, "bold"), bg='FBF6EF', fg="white")
+lab_min_txt = Label(clock, text="MIN.",font=('Time New Roman', 20, "bold"), bg='#FBF6EF', fg="Black")
 lab_min_txt.place(x=340, y=190, height=40, width=100)
 
-lab_sec = Label(clock, text="00",font=('Time New Roman', 60, "bold"), bg='EAD7C3', fg="white")
+lab_sec = Label(clock, text="00",font=('Time New Roman', 60, "bold"), bg='#EAD7C3', fg="white")
 lab_sec.place(x=560, y=50, height=110, width=100)
-lab_sec_txt = Label(clock, text="SEC.",font=('Time New Roman', 20, "bold"), bg='FBF6EF', fg="white")
+lab_sec_txt = Label(clock, text="SEC.",font=('Time New Roman', 20, "bold"), bg='#FBF6EF', fg="Black")
 lab_sec_txt.place(x=560, y=190, height=40, width=100)
 
-lab_am = Label(clock, text="00",font=('Time New Roman', 50, "bold"), bg='EAD7C3', fg="white")
+lab_am = Label(clock, text="00",font=('Time New Roman', 50, "bold"), bg='#EAD7C3', fg="white")
 lab_am.place(x=780, y=50, height=110, width=100)
-lab_am_txt = Label(clock, text="AM/PM",font=('Time New Roman', 20, "bold"), bg='FBF6EF', fg="white")
+lab_am_txt = Label(clock, text="AM/PM",font=('Time New Roman', 20, "bold"), bg='#FBF6EF', fg="Black")
 lab_am_txt.place(x=780, y=190, height=40, width=100)
 
 
 # Date
-lab_date = Label(clock, text="00",font=('Time New Roman', 60, "bold"), bg='EAD7C3', fg="white")
+lab_date = Label(clock, text="00",font=('Time New Roman', 60, "bold"), bg='#EAD7C3', fg="white")
 lab_date.place(x=120, y=270, height=110, width=100)
-lab_date_txt = Label(clock, text="DATE",font=('Time New Roman', 20, "bold"), bg='FBF6EF', fg="white")
+lab_date_txt = Label(clock, text="DATE",font=('Time New Roman', 20, "bold"), bg='#FBF6EF', fg="Black")
 lab_date_txt.place(x=120, y=410, height=40, width=100)
 
-lab_mon = Label(clock, text="00",font=('Time New Roman', 60, "bold"), bg='EAD7C3', fg="white")
+lab_mon = Label(clock, text="00",font=('Time New Roman', 60, "bold"), bg='#EAD7C3', fg="white")
 lab_mon.place(x=340, y=270, height=110, width=100)
-lab_mon_txt = Label(clock, text="MONTH",font=('Time New Roman', 20, "bold"), bg='FBF6EF', fg="white")
+lab_mon_txt = Label(clock, text="MONTH",font=('Time New Roman', 20, "bold"), bg='#FBF6EF', fg="Black")
 lab_mon_txt.place(x=340, y=410, height=40, width=100)
 
-lab_year = Label(clock, text="00",font=('Time New Roman', 60, "bold"), bg='EAD7C3', fg="white")
+lab_year = Label(clock, text="00",font=('Time New Roman', 60, "bold"), bg='#EAD7C3', fg="white")
 lab_year.place(x=560, y=270, height=110, width=100)
-lab_year_txt = Label(clock, text="YEAR",font=('Time New Roman', 20, "bold"), bg='FBF6EF', fg="white")
+lab_year_txt = Label(clock, text="YEAR",font=('Time New Roman', 20, "bold"), bg='#FBF6EF', fg="Black")
 lab_year_txt.place(x=560, y=410, height=40, width=100)
 
-lab_day = Label(clock, text="00",font=('Time New Roman', 35, "bold"), bg='EAD7C3', fg="white")
+lab_day = Label(clock, text="00",font=('Time New Roman', 35, "bold"), bg='#EAD7C3', fg="white")
 lab_day.place(x=780, y=270, height=110, width=100)
-lab_day_txt = Label(clock, text="DAY",font=('Time New Roman', 20, "bold"), bg='FBF6EF', fg="white")
+lab_day_txt = Label(clock, text="DAY",font=('Time New Roman', 20, "bold"), bg='#FBF6EF', fg="Black")
 lab_day_txt.place(x=780, y=410, height=40, width=100)
 
 
